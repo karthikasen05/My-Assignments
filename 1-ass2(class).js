3e59ac7741c3d2f3f6502807bd2e8db2223a251f
karthikasen05 <karthiga.shan@gmail.com>
2025-09-20 19:07:28 +0530

    Add files via upload
    Class-Ass1-Odd/Even

---
+++ 1-ass2(class).js
@@ -0,0 +1,19 @@
+function oddoreven(x)
+ {
+  
+  if (x/2==0) {
+    console.log("Thenumber: even")
+    
+  } 
+  else {
+
+   console.log("Thenumber :odd")
+    
+
+        
+    }
+    
+  }  
+
+oddoreven(87)
+
