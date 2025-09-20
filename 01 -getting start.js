7ec5ad0ffb127931ac9b204a1ea832186125b288
karthikasen05 <karthiga.shan@gmail.com>
2025-09-20 19:04:08 +0530

    Add files via upload
    Day 1 -Ass1

---
+++ 01 -getting start.js
@@ -0,0 +1,9 @@
+var firstname = "Karthika"
+console.log(firstname) 
+console.log(typeof firstname)
+var companyname ="indium"
+console.log(companyname)
+console.log(typeof companyname)
+var mobilenumber = 9444257555
+console.log(mobilenumber)
+console.log(typeof mobilenumber)
