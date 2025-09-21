4e1bcc76ed7e0f0b090a7f5eaf6d20a66a072853
karthikasen05 <karthiga.shan@gmail.com>
2025-09-21 15:06:03 +0530

    Day 2-Numbertype-HomeAss1

---
+++ 2-NumberType-homeAss2.js
@@ -0,0 +1,17 @@
+
+let x=-25;
+function number()
+ {
+    if (x>=0) 
+        console.log("The number is Postive");
+    else if (x<=0) 
+        console.log("The number is Negative");
+
+        else {
+            console.log("The Number is Neutral");
+            
+        
+    }
+        
+    }
+number();
