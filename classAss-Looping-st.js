237235b9b8a2884aee5300e4c3d2d81cacc1fae6
karthikasen05 <karthiga.shan@gmail.com>
2025-09-24 20:04:49 +0530

    classwork-looping

---
+++ classAss-Looping-st.js
@@ -0,0 +1,25 @@
+function printodd() {
+    
+
+for (let i = 0; i <= 25 ; i++) {
+
+    if (i%2!=0) {
+        console.log (i);
+        
+    }
+
+        
+    }
+}
+    printodd();
+
+    // ternary operator //
+    
+    let x = -25;
+    function num() {
+        let message = x >= 0 ? "number is postive" : "number is negative";
+        console.log (message);
+
+        
+    }
+    num();
