e20d2a7acfc73f7dd7bf29c1c4eae4805f857638
karthikasen05 <karthiga.shan@gmail.com>
2025-10-21 16:56:44 +0530

    Update alert-frame-interaction-HomeAss.spec.js

--- alert-frame-interaction-HomeAss.spec.js
+++ alert-frame-interaction-HomeAss.spec.js
@@ -3,11 +3,11 @@ test(`to handle alerts`,async({page})=>{
     await page.goto(`https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm`);
     await page.reload();
     //syntax for alert - //Event Listener //
-    const frameloc =page.frameLocator(`//iframe[@id='iframeResult']`);
      page.on('dialog', async (dialog) => {
     console.log(`Alert message: ${dialog.message()}`);
     await dialog.accept(); // Accept the alert (equivalent to pressing OK)
   });
+   const frameloc =page.frameLocator(`//iframe[@id='iframeResult']`); 
    await frameloc.locator(`//button[text()='Try it']`).click();
 
    const result = await frameloc.locator('#demo').textContent();
@@ -15,3 +15,4 @@ test(`to handle alerts`,async({page})=>{
 
 
     })
+
